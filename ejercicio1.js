6c0f27eb4594a20d1d9c7ce4e9c29edc7e6db105
Martin-Salomon <martinsalomon11@gmail.com>
2022-03-09 21:23:44 -0300

    ultima modificacion Martin

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 console.log('Este es el ej 1 y lo estamos modificando al mismo tiempo!');
-=======
 console.log('Este es el ej 1!, ¡VAMOS BIEN!');
->>>>>>> 53d4acdb399cf40148ca96e63b02ef27828bceef
